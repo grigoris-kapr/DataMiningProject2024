9d6444e8e0880b443fe4d5e1d875fb3df74086ac
Gregory <grigoriskapr@gmail.com>
2025-01-04 22:49:48 +0200

    Made small changes to hierarchical (with same underlying alg.)

--- hierarchical.py
+++ hierarchical.py
@@ -22,11 +22,11 @@ def dist(x, y):
                 sum_dist += abs( x[attr] - y[attr] ) 
     return sum_dist / sum_d 
 
-def find_min_distance(distance_matrix, n):
+def find_min_distance(distance_matrix):
     i_min = -1
     j_min = -1
     minimum = np.inf
-    for i in range(n):
+    for i in range(len(distance_matrix)):
         for j in range(i):
             if distance_matrix[i][j] < minimum:
                 i_min = i
@@ -59,40 +59,64 @@ def hierarcical(data, clusters_num, type=True, clusters=None):
         clusters = [ [i] for i in range(n) ]
     # merging loop
     while len(clusters) > clusters_num:
-        i_min, j_min = find_min_distance(distance_matrix, n)
+        # find clusters to merge: j will be merged into i
+        i_min, j_min = find_min_distance(distance_matrix)
+        # always merge smaller cluster onto the bigger one for more efficient distance_matrix calculations
+        if len(clusters[j_min]) > len(clusters[i_min]):
+            temp = i_min
+            i_min = j_min
+            j_min = temp
 
+        # merge clusters and keep a copy of the one that was merged into the other
         clusters[i_min].extend(clusters[j_min])
+        merged_cluster = clusters[j_min].copy()
         del clusters[j_min]
 
         # delete cluster j from distance matrix (i will be overwritten)
-        np.delete(distance_matrix, j_min, 0) # delete j-th row
-        np.delete(distance_matrix, j_min, 1) # delete j-th col
+        distance_matrix = np.delete(distance_matrix, j_min, 0) # delete j-th row
+        distance_matrix = np.delete(distance_matrix, j_min, 1) # delete j-th col
+
+        # in case i_min > j_min, reference to [i_min] will now be broken (might even cause OOB error)
+        # in that case, i_min should be reduced by 1 (because the row/col deleted came before it)
+        if i_min > j_min: i_min -= 1
 
         # update matrix for single-link
         if type == True:
             # update i-th row of distance matrix
             for col in range(i_min):
-                # distance is minimum / maximum between every 
-                # pair of points in clusters i_min & col
-                distance_matrix[i_min][col] = min( full_distance_matrix[p1][p2] for p2 in clusters[col] for p1 in clusters[i_min])
+                # distance is minimum between every pair of points in clusters[i_min] & clusters[col]
+                # clusters[i_min] is comprised of the old clusters[i_min] and the merged_cluster
+                # min of the old clusters[i_min] is old_best. only need to calculate new_best from merged_cluster and take min of both
+                old_best = distance_matrix[i_min][col]
+                new_best = min(full_distance_matrix[p1][p2] for p2 in clusters[col] for p1 in merged_cluster)
+                distance_matrix[i_min][col] = min(old_best, new_best)
             # update i-th col of distance matrix
-            for row in range(i_min+1, n):
-                # distance is minimum / maximum between every 
-                # pair of points in clusters i_min & col
-                distance_matrix[row][i_min] = min( full_distance_matrix[p1][p2] for p2 in clusters[i_min] for p1 in clusters[row])
+            for row in range(i_min+1, len(clusters)):
+                # distance is minimum between every pair of points in clusters[i_min] & clusters[row]
+                # clusters[i_min] is comprised of the old clusters[i_min] and the merged_cluster
+                # min of the old clusters[i_min] is old_best. only need to calculate new_best from merged_cluster and take min of both
+                old_best = distance_matrix[row][i_min]
+                new_best = min( full_distance_matrix[p1][p2] for p2 in merged_cluster for p1 in clusters[row])
+                distance_matrix[row][i_min] = min(old_best, new_best)
         
         # update matrix for complete link
         elif type == False:
             # update i-th row of distance matrix
             for col in range(i_min):
-                # distance is minimum / maximum between every 
-                # pair of points in clusters i_min & col
-                distance_matrix[i_min][col] = max( full_distance_matrix[p1][p2] for p2 in clusters[col] for p1 in clusters[i_min])
+                # distance is maximum between every pair of points in clusters[i_min] & clusters[col]
+                # clusters[i_min] is comprised of the old clusters[i_min] and the merged_cluster
+                # min of the old clusters[i_min] is old_best. only need to calculate new_best from merged_cluster and take min of both
+                old_best = distance_matrix[i_min][col]
+                new_best = max(full_distance_matrix[p1][p2] for p2 in clusters[col] for p1 in merged_cluster)
+                distance_matrix[i_min][col] = max(old_best, new_best)
             # update i-th col of distance matrix
-            for row in range(i_min+1, n):
-                # distance is minimum / maximum between every 
-                # pair of points in clusters i_min & col
-                distance_matrix[row][i_min] = max( full_distance_matrix[p1][p2] for p2 in clusters[i_min] for p1 in clusters[row])
+            for row in range(i_min+1, len(clusters)):
+                # distance is maximum between every pair of points in clusters[i_min] & clusters[row]
+                # clusters[i_min] is comprised of the old clusters[i_min] and the merged_cluster
+                # min of the old clusters[i_min] is old_best. only need to calculate new_best from merged_cluster and take min of both
+                old_best = distance_matrix[row][i_min]
+                new_best = max( full_distance_matrix[p1][p2] for p2 in merged_cluster for p1 in clusters[row])
+                distance_matrix[row][i_min] = max(old_best, new_best)
 
         # if type is neither, throw error
         else:
@@ -102,7 +126,7 @@ def hierarcical(data, clusters_num, type=True, clusters=None):
 
 
 # read data into data
-data = pd.read_csv('train.csv', sep=',',header=0).values
+data = pd.read_csv('test_2000.csv', sep=',',header=0).values
 
 # drop unused data
 data = np.delete(data, [0, 1, 24], 1)
@@ -113,4 +137,7 @@ for i in range(len(data)):
         if attr >= 6 and attr <= 19 and data[i][attr] == 0:
             data[i][attr] = None
 
+start = time.time()
 hierarcical(data, 5)
+end = time.time()
+print(end-start)
